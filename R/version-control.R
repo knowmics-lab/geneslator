f5298c5cd8d15a9f3c3f05571c1c77b7acf1b1d4
Giovanni Micale <gmgmicky@gmail.com>
2026-02-07 13:31:02 +0100

    Updated links to Github release

--- R/version-control.R
+++ R/version-control.R
@@ -5,7 +5,7 @@ NULL
 #' @keywords internal
 #' @noRd
 .get_remote_db_version <- function(organism) {
-    url <- paste0("https://github.com/GMicale/geneslator/releases/download/",
+    url <- paste0("https://github.com/knowmics-lab/geneslator/releases/download/",
     "GeneslatorDb/",organism,".version")
     return(read.table(url(url))$V1)
 }
